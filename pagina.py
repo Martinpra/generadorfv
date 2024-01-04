c72dd01acd6be69be112a2a5a042639322708e66
Martinpra <42524452+Martinpra@users.noreply.github.com>
2024-01-04 11:35:12 -0300

    Add files via upload

---
+++ pagina.py
@@ -0,0 +1,9 @@
+# app.py
+import streamlit as st
+
+def main():
+    st.title("Mi Aplicación de Streamlit")
+    st.write("¡Hola, esta es una aplicación de Streamlit!")
+
+if __name__ == "__main__":
+    main()
